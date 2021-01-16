a17cbda8f44aa2487666d00376ad368d269b6901
Wei Ding <ding_web@yahoo.co.jp>
2021-01-16 14:25:57 -0500

    add sub page

---
+++ src/app/sub-field/sub-field.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SubFieldComponent } from './sub-field.component';
+
+describe('SubFieldComponent', () => {
+  let component: SubFieldComponent;
+  let fixture: ComponentFixture<SubFieldComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SubFieldComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SubFieldComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
